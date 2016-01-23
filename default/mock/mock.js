c244ecfe3cade223bcc0bf2b04b699b9b89034d4
dingwei <dingwei@ufish.io>
2016-01-23 12:25:00 +0800

    fix bug and support webpack HMR

--- default/mock/mock.js
+++ default/mock/mock.js
@@ -1,36 +1,30 @@
 import Mock from 'mockjs';
 var url = require('url');
 
-const enableMock = false;
-
+const enableMock = true;
 if (enableMock) {
-    // if jQuery exists
-    if ($) {
-        $.ajaxSetup({
-            dataFilter(data, dataType) {
-                try {
-                    return JSON.parse(data);
-                } catch (Ex) {
-                    return data;
-                }
+    $.ajaxSetup({
+        dataFilter(data, dataType) {
+            try {
+                return JSON.parse(data);
+            } catch (Ex) {
+                return data;
             }
-        })
-    }
+        }
+    });
 
     Mock.setup({
         timeout: '200-600',
         responseType: 'json'
     })
-    Mock.mock(/\s*/, (options) => {
+    Mock.mock(/\s*^(?!.*hot-update)/, (options) => {
         let _url = url.parse(options.url);
-        console.log(`request url: ${_url.pathname}`);
-        if (_url.pathname.indexOf('hot-update') == -1) {
-            if (_url.pathname[0] == '/') {
-                _url.pathname = _url.pathname.substring(1);
-            }
-            var mockData = require('./' + _url.pathname.replace(/\//g, '-') + '.json');
-            return Mock.mock(mockData);
+        console.log(`mock request: ${_url.pathname}`);
+        if (_url.pathname[0] == '/') {
+            _url.pathname = _url.pathname.substring(1);
         }
+        var mockData = require('./' + _url.pathname.replace(/\//g, '-') + '.json');
+        return Mock.mock(mockData);
     })
 }
 

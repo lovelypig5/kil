1836f13bcb776708bfe72a66ab3b12cdad1b3a9a
dingwei <dingwei@ufish.io>
2016-01-26 10:50:28 +0800

    Merge branch 'dev'

--- default/pack.js
+++ default/pack.js
@@ -15,6 +15,20 @@
             //     name: "common"
             // })
         ],
+        devServer: {
+            proxy: {
+                '/*': {
+                    target: 'activity.gongzuojihui.com',
+                    secure: false,
+                    bypass: function(req, res, proxyOptions) {
+                        if (req.headers.accept.indexOf('html') !== -1) {
+                            console.log('Skipping proxy for browser request.');
+                            return '/index.html';
+                        }
+                    },
+                },
+            },
+        },
         module: {
             loaders: []
         },

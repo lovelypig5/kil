0411687eef1ae9581a32508bac8da4383b785bb1
dingwei <dingwei@ufish.io>
2016-02-01 12:11:27 +0800

    support react hot reload extract a babel file to produce the query for babel loader

---
+++ babel.js
@@ -0,0 +1,25 @@
+/**
+ * load babel loader query string, presets and plugins
+ * @type {[type]}
+ */
+var utils = require('./utils'),
+    conf = utils.loadConfig();
+
+var babelQueryStr = {};
+babelQueryStr.presets = [require.resolve('babel-preset-es2015')];
+babelQueryStr.plugins = [
+    [require.resolve('babel-plugin-transform-runtime')]
+];
+if (conf.react === true) {
+    babelQueryStr.presets.push(require.resolve('babel-preset-react'));
+    babelQueryStr.plugins.push([require.resolve('babel-plugin-react-transform'), {
+        transforms: [{
+            transform: 'react-transform-hmr',
+            imports: ['react'],
+            locals: ['module']
+        }],
+    }]);
+}
+babelQueryStr.cacheDirectory = true;
+
+module.exports = JSON.stringify(babelQueryStr);

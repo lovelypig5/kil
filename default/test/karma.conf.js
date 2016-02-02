18324546bf0419a2a21f6dc72f575b4f27f9664b
dingwei <dingwei@ufish.io>
2016-02-02 10:05:16 +0800

    change test config file

--- default/test/karma.conf.js
+++ default/test/karma.conf.js
@@ -1,11 +1,25 @@
-var webpack = require(`webpack`);
-var pack_config = require('./kil.pack.js');
+var path = require('path');
+var webpackPath = 'webpack';
+var plugins = ['karma-webpack', 'karma-chrome-launcher', 'karma-phantomjs-launcher', 'karma-mocha', 'karma-mocha-reporter', 'karma-coverage'];
+if (process.env.KIL_HOME) {
+    webpackPath = path.resolve(process.env.KIL_HOME, 'node_modules', webpackPath);
+    plugins = plugins.map(function(plugin) {
+        return require(path.resolve(process.env.KIL_HOME, 'node_modules', plugin));
+    })
+} else {
+    plugins = plugins.map(function(plugin) {
+        return require(plugin);
+    });
+}
+
+var webpack = require(webpackPath);
+var pack_config = require('../pack');
 delete pack_config.entry;
 delete pack_config.output;
 // quick source map, dev tool for rebuild performance
 pack_config.devtool = 'eval';
 // add plugin
-pack_config.plugins.push(new webpack.HotModuleReplacementPlugin());
+pack_config.plugins = [new webpack.HotModuleReplacementPlugin()];
 
 module.exports = function(config) {
     config.set({
@@ -28,14 +42,6 @@ module.exports = function(config) {
         webpackMiddleware: {
             noInfo: true
         },
-        plugins: [
-            'karma-webpack',
-            'karma-chrome-launcher',
-            'karma-phantomjs-launcher',
-            'karma-mocha',
-            'karma-mocha-reporter',
-            'karma-coverage',
-        ]
-
+        plugins: plugins
     });
 };
